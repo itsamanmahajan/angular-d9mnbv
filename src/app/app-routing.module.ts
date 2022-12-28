59b3a2fc0ecef0ccf876c61b7048b680cfb155d4
itsamanmahajan <102498771+itsamanmahajan@users.noreply.github.com>
2022-12-28 13:17:46 +0530

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { BooksComponent } from './books/books.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: CartComponent,
-  },
-  {
     path: 'book',
-    component: BookComponent,
+    component: BooksComponent,
   },
 ];
 @NgModule({
